a3e1bac71b3035f7df940a06d01c5305daafb07c
Haruka Goto <harubj6999@gmail.com>
2020-03-10 03:14:47 +0900

    update:toppage

--- js/main.js
+++ js/main.js
@@ -54,57 +54,61 @@ for (var i=0;i<cardItems.length;i++){
 };
 
 var thumbnailItems = [
-  {name: 'freee1weekIntern',extension: 'png'},
-  {name: 'freee1dayIntern',extension: 'png'},
-  {name: 'a-teamIntern',extension: 'png'},
-  {name: 'portfoliosite',extension: 'png'},
-  {name: 'schoolFestivalPreSite',extension: 'png'},
-  {name: 'diagnosticChart',extension: 'png'},
-  {name: 'schoolFestivalSite',extension: 'png'},
-  {name: 'ultraSole',extension: 'png'},
-  {name: 'fasTag',extension: 'png'},
-  {name: 'moneyManegement',extension: 'png'},
-  {name: 'EdelRöteJazzOrchesterHP',extension: 'png'},
-  {name: 'freeeIntern',extension: 'png'},
-  {name: 'motioCapture',extension: 'png'},
-  {name: 'blockgame',extension: 'png'},
-  {name: 'designAnthropology',extension: 'png'}
+  {name: 'freee1weekIntern',extension: 'png',description:'freee株式会社デザイナー',description2:'1weekインターン'},
+  {name: 'freee1dayIntern',extension: 'png',description:'freee株式会社デザイナー',description2:'1dayインターン'},
+  {name: 'a-teamIntern',extension: 'png',description:'株式会社エイチームデザイナー',description2:'3daysインターン'},
+  {name: 'portfoliosite',extension: 'png',description:'ポートフォリオサイト',description2:''},
+  {name: 'schoolFestivalPreSite',extension: 'png',description:'学祭学内向けプレサイト',description2:''},
+  {name: 'diagnosticChart',extension: 'png',description:'学祭学内向け診断チャート',description2:''},
+  {name: 'schoolFestivalSite',extension: 'png',description:'学祭学外向けサイト',description2:''},
+  {name: 'ultraSole',extension: 'png',description:'IoT×インソール',description2:''},
+  {name: 'fasTag',extension: 'png',description:'メモの備わった電子書籍サービス',description2:'FasTag'},
+  {name: 'moneyManegement',extension: 'png',description:'家計簿アプリ',description2:''},
+  {name: 'EdelRöteJazzOrchesterHP',extension: 'png',description:'所属バンドのホームページ',description2:''},
+  {name: 'freeeIntern',extension: 'png',description:'freee株式会社エンジニア',description2:'2weeksインターン'},
+  {name: 'motioCapture',extension: 'png',description:'モーションキャプチャー',description2:'を用いたゲーム製作'},
+  {name: 'blockgame',extension: 'png',description:'対戦型ブロックゲーム',description2:''}
 ];
 var thumbnail;
 var position;
 var bgColor;
 var num;
-function thumbnailValue(i,name,extension) {
-  if(i / 2 == 0) {
-    position = 'upperLeft';
-  }else {
-    position = 'lowerLeft';
-  }
-  num = Math.floor(i / 3) + (i % 3);
-  if (num >= 3) {
-    num = num % 3;
-  }
-  switch (num){
-    case 0:
-      bgColor = 'blue';
-      break;
-    case 1:
-      bgColor = 'pink';
-      break;
-    case 2:
-      bgColor = 'lblue';
-      break;
-  }
+function thumbnailValue(i,name,extension,description,description2) {
+  // if(i % 2 == 0) {
+  //   position = 'upperLeft';
+  // }else {
+  //   position = 'lowerLeft';
+  // }
+  // num = Math.floor(i / 3) + (i % 3);
+  // if (num >= 3) {
+  //   num = num % 3;
+  // }
+  // switch (num){
+  //   case 0:
+  //     bgColor = 'blue';
+  //     break;
+  //   case 1:
+  //     bgColor = 'pink';
+  //     break;
+  //   case 2:
+  //     bgColor = 'lblue';
+  //     break;
+  // }
 
   thumbnail = [
     '<div class="thumbnail">'+
-      '<div class="thumbnail-foundation '+position+' '+bgColor+'"></div>'+
-      '<img src="./image/thumbnail/'+name+'.'+extension+'" alt="'+name+'のサムネイル" class="thumbnail-img">'+
+      // '<div class="thumbnail-foundation '+position+' '+bgColor+'"></div>'+
+      '<img src="./image/thumbnail/'+name+'.'+extension+'" alt="'+description+description2+'" class="thumbnail-img">'+
+      '<div class="thumbnail-mask">'+
+        '<div class="thumbnail-caption">'+
+          description+'<br>'+description2+
+        '</div>'+
+      '</div>'+
     '</div>'
   ];
   return thumbnail;
 };
 
 for (var i=0;i<thumbnailItems.length;i++){
-  document.getElementById('thumbnailContainer').insertAdjacentHTML('beforeend', thumbnailValue(i,thumbnailItems[i].name,thumbnailItems[i].extension));
+  document.getElementById('thumbnailContainer').insertAdjacentHTML('beforeend', thumbnailValue(i,thumbnailItems[i].name,thumbnailItems[i].extension,thumbnailItems[i].description,thumbnailItems[i].description2));
 };
